0b05285bc5faadb5bc15585d6892175591b32894
rafaelmanicardi <rafamanicard@hotmail.com>
2022-07-19 21:58:03 -0300

    coding..

--- src/GeradorDeFigurinhas.java
+++ src/GeradorDeFigurinhas.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,14 +16,20 @@ public class GeradorDeFigurinhas {
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
         int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
         //  copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
 
         // escrever uma frase na nova imagem
+        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
 
         //  escrever a nova imagem em um arquivo
 
-
     }
-    
+    public static void main(String[] args) throws Exception {
+        GeradorDeFigurinhas geradora = new GeradorDeFigurinhas();
+        geradora.criar();
+    }    
 }
