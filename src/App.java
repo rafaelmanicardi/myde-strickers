b2b3d46c61e640f1d93aa34ac5713bd14efd1645
rafaelmanicardi <rafamanicard@hotmail.com>
2022-07-20 22:03:14 -0300

    committtt

--- src/App.java
+++ src/App.java
@@ -1,24 +1,26 @@
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         //  fazer uma conexão HTTP e buscar os top 250 filmes
-        //String url = "https://imdb-api.com/en/API/Top250Movies/k_9zt1zkfd";
-        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_9zt1zkfd";
+        ExtratorDeConteudoDoIMDB extrator = new ExtratorDeConteudoDoIMDB();
+
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();     
 
         ClienteHttp http = new ClienteHttp();
         String json = http.buscaDados(url);
 
         //  Exibir e manipular os dados
-        ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
         List<Conteudo> conteudos = extrator.extraiConteudos(json);
          
         //for (Map<String,String> filme : listaDeFilmes) {
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 3; i++){
 
             Conteudo conteudo = conteudos.get(i);
 
