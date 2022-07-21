7e6b392b40ab4fd2c7f16a4ef6a6c5dc4bc78c05
rafaelmanicardi <rafamanicard@hotmail.com>
2022-07-20 21:21:54 -0300

    commit

--- src/App.java
+++ src/App.java
@@ -13,13 +13,9 @@ public class App {
 
         //  fazer uma conexão HTTP e buscar os top 250 filmes
         //String url = "https://imdb-api.com/en/API/Top250Movies/k_9zt1zkfd";
-        String url = "https://api.nasa.gov/planetary/apod?api_key=HlQnYCLJ0Lp9j7qft1doxfMP9l6adZHEfKdmMCN7";
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=HlQnYCLJ0Lp9j7qft1doxfMP9l6adZHEfKdmMCN7";
 
-        URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request,BodyHandlers.ofString());
-        String body = response.body();
+
 
         //  extrair apenas os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
@@ -28,15 +24,15 @@ public class App {
         //  Exibir e manipular os dados
          
         //for (Map<String,String> filme : listaDeFilmes) {
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 10; i++){
 
-            Map<String, String> filme = listaDeConteudos.get(i);
+            Map<String, String> conteudo = listaDeConteudos.get(i);
 
-            //String urlImagem = filme.get("image")
-            String urlImagem = filme.get("url")
+            String urlImagem = conteudo.get("image")
+            //String urlImagem = conteudo.get("url")
             .replaceAll("(@+)(.*).jpg$","$1.jpg");
 
-            String titulo = filme.get("title");
+            String titulo = conteudo.get("title");
 
             InputStream inputStream = new URL(urlImagem).openStream();
             String nomeArquivo = "saida/" + titulo + ".png";
@@ -46,9 +42,9 @@ public class App {
 
             System.out.println(titulo);
             System.out.println();
-            /*System.out.println(filme.get("imDbRating"));
+            /*System.out.println(conteudo.get("imDbRating"));
             System.out.println();          
-            System.out.println(filme.get("year"));
+            System.out.println(conteudo.get("year"));
             System.out.println();*/
         }
     }
