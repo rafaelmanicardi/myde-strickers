9c5c72eff0b31fbd141a50007ceb1f7eb692907d
rafaelmanicardi <rafamanicard@hotmail.com>
2022-07-20 21:54:21 -0300

    commit

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -13,35 +8,30 @@ public class App {
 
         //  fazer uma conexão HTTP e buscar os top 250 filmes
         //String url = "https://imdb-api.com/en/API/Top250Movies/k_9zt1zkfd";
-        //String url = "https://api.nasa.gov/planetary/apod?api_key=HlQnYCLJ0Lp9j7qft1doxfMP9l6adZHEfKdmMCN7";
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
+        ClienteHttp http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
-
-        //  extrair apenas os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeConteudos = parser.parse(body);
-        
         //  Exibir e manipular os dados
+        ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
          
         //for (Map<String,String> filme : listaDeFilmes) {
         for (int i = 0; i < 10; i++){
 
-            Map<String, String> conteudo = listaDeConteudos.get(i);
+            Conteudo conteudo = conteudos.get(i);
 
-            String urlImagem = conteudo.get("image")
-            //String urlImagem = conteudo.get("url")
-            .replaceAll("(@+)(.*).jpg$","$1.jpg");
-
-            String titulo = conteudo.get("title");
-
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "saida/" + titulo + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
 
             GeradorDeFigurinhas geradora = new GeradorDeFigurinhas();
             geradora.criar(inputStream, nomeArquivo);
 
-            System.out.println(titulo);
+            System.out.println(conteudo.getTitulo());
             System.out.println();
+
+
             /*System.out.println(conteudo.get("imDbRating"));
             System.out.println();          
             System.out.println(conteudo.get("year"));
---
+++ src/ExtratorDeConteudoDaNasa.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoDaNasa {
+
+    public List<Conteudo> extraiConteudos (String json){
+
+        // extrair apenas os dados que interessam(titulo, poster, classificacao)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        //  popular a lista de conteudos
+        for (Map<String, String> atributos : listaDeAtributos) {
+        String titulo = atributos.get("title");
+        String urlImagem = atributos.get("url");    
+        
+        var conteudo =new Conteudo(titulo, urlImagem);  
+        
+        conteudos.add(conteudo);
+            
+        }
+
+        //retorno
+        return conteudos;
+
+    }
+    
+}
