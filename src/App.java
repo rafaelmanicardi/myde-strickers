20be31b928f68220d7ab63584a607b30b062204f
rafaelmanicardi <rafamanicard@hotmail.com>
2022-07-20 22:13:02 -0300

    commit again

--- src/App.java
+++ src/App.java
@@ -7,11 +7,11 @@ public class App {
 
         //  fazer uma conexão HTTP e buscar os top 250 filmes
 
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_9zt1zkfd";
-        ExtratorDeConteudoDoIMDB extrator = new ExtratorDeConteudoDoIMDB();
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_9zt1zkfd";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
-        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        //ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();     
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();     
 
         ClienteHttp http = new ClienteHttp();
         String json = http.buscaDados(url);
